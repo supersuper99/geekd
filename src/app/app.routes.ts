e8a17646cffdf4ec92062e43e1ec34f07d74382c
eric <eandrews100@gmail.com>
2025-09-30 19:57:42 -0500

    first com

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,16 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'signup',
+    loadComponent: () => import('./signup/signup.page').then( m => m.SignupPage)
+  },
+  {
+    path: 'createprofile',
+    loadComponent: () => import('./createprofile/createprofile.page').then( m => m.CreateprofilePage)
+  },
 ];
